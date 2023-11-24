52e0715b6ad5895139453bb4443c88a0b1e01426
Arnab Gupta <arnabgupta1111@gmail.com>
2023-11-24 19:50:50 +0600

    project setup done

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    colors: {
+      navy: "#1B2850",
+      orange: "#FF792E",
+      green: "#13ce66",
+      "light-sky": "#FAFBFE",
+      gray: "#7B7B8A",
+      "gray-light": "#d3dce6",
+    },
+    fontFamily: {
+      crimson: ["Crimson Pro", "serif"],
+    },
+  },
+  plugins: [require("daisyui")],
+};
