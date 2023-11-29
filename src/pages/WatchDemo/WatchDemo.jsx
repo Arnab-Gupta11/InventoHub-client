9cd0ba04372e934e2686409acf56182ee688abbb
Arnab Gupta <arnabgupta1111@gmail.com>
2023-11-29 21:18:40 +0600

    Watch video page work done

--- src/pages/WatchDemo/WatchDemo.jsx
+++ src/pages/WatchDemo/WatchDemo.jsx
@@ -1,7 +1,26 @@
+import YouTube from "react-youtube";
 const WatchDemo = () => {
+  const videoId = "qQGT74k-g1E";
+
+  // Options for the YouTube player
+  const opts = {
+    height: "500",
+    width: "800",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  // Callback function to handle events from the YouTube player
+  const onReady = (event) => {
+    // access to player in all event handlers via event.target
+    event.target.pauseVideo();
+  };
   return (
-    <div>
-      <h2>watch</h2>
+    <div className="px-5  p-7 mb-10 rounded-md max-w-screen-xl mx-auto min-h-screen mt-6">
+      <div className="border bg-[#FAFBFE] p-7">
+        <YouTube videoId={videoId} opts={opts} onReady={onReady} />
+      </div>
     </div>
   );
 };
